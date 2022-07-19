0ef2fdcae986caf9d68f7c3be559c28bf87c8304
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-07-19 16:33:46 +0700

    [ADD]Loading

--- src/components/PageLoader.vue
+++ src/components/PageLoader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="!isloaded" class="flex items-center justify-center fixed z-50 top-0 left-0 w-screen h-screen">
+    <div v-if="!isloaded" class="flex items-center justify-center fixed z-50 top-0 left-0 w-screen h-screen bg-white">
          <div style="border-top-color:transparent"
             class="w-16 h-16 border-4 border-blue-400 border-solid rounded-full animate-spin"></div>
     </div>
--- src/views/RecipeDetail.vue
+++ src/views/RecipeDetail.vue
@@ -1,62 +1,82 @@
 <template>
-    <div>
-        <div class="p-3 relative">
-            <img class="w-full h-80 object-cover rounded-xl" :src="recipe.thumb" :alt="'photo ' + recipe.title">
+  <div>
+    <div class="p-3 relative">
+      <img
+        class="w-full h-80 object-cover rounded-xl"
+        :src="recipe.thumb"
+        :alt="'photo ' + recipe.title"
+      />
 
-            <router-link to="/" class="w-10 h-10 flex items-center justify-center rounded-md bg-white absolute -mt-[19rem] ml-3">
-                <i class='bx bx-chevron-left text-3xl text-[#FF9F95]'></i>
-            </router-link>
-        </div>
-        
-        
-
-       <div class="px-5">
-            <h2 class="recipe_title h-[60px]">{{recipe.title}}</h2>
-            <h3>{{}}</h3>
-            <div class="flex items-center justify-center gap-6 mt-6">
-
-                <div class="flex-col flex items-center justify-center bg-green-100 w-24 h-24 rounded-xl">
-                         <i class="bx bx-time-five text-green-500 text-5xl"></i>
-                         <span class="text-sm text-green-500 mt-1 font-semibold">{{recipe.times}}</span>
-                </div>
-
-                <div class="flex-col flex items-center justify-center bg-blue-100 w-24 h-24 rounded-xl">
-                         <i class="bx bx-food-menu text-blue-500 text-5xl"></i>
-                         <span class="text-sm text-blue-500 mt-1 font-semibold">{{recipe.dificulty}}</span>
-                </div>
-
-                <div class="flex-col flex items-center justify-center bg-orange-100 w-24 h-24 rounded-xl">
-                         <i class="bx bx-bowl-rice text-orange-500 text-5xl"></i>
-                         <span class="text-sm text-orange-500 mt-1 font-semibold">{{recipe.servings}}</span>
-                </div>
-
-            </div>
-             <p class="text-base text-gray-500 leading-relaxed mt-6">{{recipe.needItem}}</p>
-       </div>
+      <router-link
+        to="/"
+        class="w-10 h-10 flex items-center justify-center rounded-md bg-white absolute -mt-[19rem] ml-3"
+      >
+        <i class="bx bx-chevron-left text-3xl text-[#FF9F95]"></i>
+      </router-link>
     </div>
+
+    <div class="px-5">
+      <h2 class="recipe_title h-[60px]">{{ recipe.title }}</h2>
+      <h3>{{}}</h3>
+      <div class="flex items-center justify-center gap-6 mt-6">
+        <div
+          class="flex-col flex items-center justify-center bg-green-100 w-24 h-24 rounded-xl"
+        >
+          <i class="bx bx-time-five text-green-500 text-5xl"></i>
+          <span class="text-sm text-green-500 mt-1 font-semibold">{{
+            recipe.times
+          }}</span>
+        </div>
+
+        <div
+          class="flex-col flex items-center justify-center bg-blue-100 w-24 h-24 rounded-xl"
+        >
+          <i class="bx bx-food-menu text-blue-500 text-5xl"></i>
+          <span class="text-sm text-blue-500 mt-1 font-semibold">{{
+            recipe.dificulty
+          }}</span>
+        </div>
+
+        <div
+          class="flex-col flex items-center justify-center bg-orange-100 w-24 h-24 rounded-xl"
+        >
+          <i class="bx bx-bowl-rice text-orange-500 text-5xl"></i>
+          <span class="text-sm text-orange-500 mt-1 font-semibold">{{
+            recipe.servings
+          }}</span>
+        </div>
+      </div>
+      <p class="text-base text-gray-500 leading-relaxed mt-6">
+        {{ recipe.needItem }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-import PageLoader from '../components/PageLoader.vue'
+import axios from "axios";
+import PageLoader from "../components/PageLoader.vue";
 
 export default {
-    components:{PageLoader},
-    data() {
-        return {
-            recipe: [],
-        }
+  components: { PageLoader },
+  data() {
+    return {
+      recipe: [],
+    };
+  },
+  methods: {
+    setRecipe(data) {
+      this.recipe = data;
     },
-    methods: {
-        setRecipe(data) {
-            this.recipe = data
-        }
-    },
-    mounted() {
-        axios
-            .get("https://masak-apa-tomorisakura.vercel.app/api/recipe/" + this.$route.params.key)
-            .then((response) => this.setRecipe(response.data.results))
-            .catch((error) => console.log(error))
-    }
-}
+  },
+  mounted() {
+    axios
+      .get(
+        "https://masak-apa-tomorisakura.vercel.app/api/recipe/" +
+          this.$route.params.key
+      )
+      .then((response) => this.setRecipe(response.data.results))
+      .catch((error) => console.log(error));
+  },
+};
 </script>
